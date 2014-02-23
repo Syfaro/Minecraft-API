c3567b28d0d66995e740390207b24b25276e705a
Syfaro <syfaro@me.com>
2014-02-22 19:13:04 -0600

    tutorial

--- minecraft.py
+++ minecraft.py
@@ -15,11 +15,11 @@ import requests
 def jsonp(func):
     @wraps(func)
     def decorated_function(*args, **kwargs):
-        callback = request.args.get('callback', False)
+        callback = request.args.get("callback", False)
         if callback:
             data = str(func(*args, **kwargs).data)
-            content = str(callback) + '(' + data + ')'
-            mimetype = 'application/javascript'
+            content = str(callback) + "(" + data + ")"
+            mimetype = "application/javascript"
             return current_app.response_class(content, mimetype=mimetype)
         else:
             return func(*args, **kwargs)
@@ -36,10 +36,15 @@ def handle_invalid_usage(error):
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route('/stats')
+@app.route("/tutorial")
+def tutorial():
+    return render_template("howto.html")
+
+
+@app.route("/stats")
 @jsonp
 def stats():
     try:
@@ -51,67 +56,67 @@ def stats():
         result = 0
 
     return jsonify({
-        'stats': int(result)
+        "stats": int(result)
     })
 
 
-@app.route('/versions')
+@app.route("/versions")
 @jsonp
 def get_versions():
     try:
-        versions = get_cache_item('versions')
+        versions = get_cache_item("versions")
     except:
         json = requests.get(
-            'http://s3.amazonaws.com/Minecraft.Download/versions/versions.json').json()
-        set_cache_item('versions', json)
+            "http://s3.amazonaws.com/Minecraft.Download/versions/versions.json").json()
+        set_cache_item("versions", json)
 
         versions = {}
-        versions['time'] = time()
-        versions['value'] = json
+        versions["time"] = time()
+        versions["value"] = json
 
     output = {}
 
-    output['status'] = 'success'
-    output['versions'] = []
-    output['time'] = versions['time']
+    output["status"] = "success"
+    output["versions"] = []
+    output["time"] = versions["time"]
 
-    versions = versions['value']
+    versions = versions["value"]
 
     bykey = {}
 
-    for version in versions['versions']:
+    for version in versions["versions"]:
         v = {}
-        v['id'] = version['id']
-        v['time'] = version['time']
-        v['niceTime'] = format_date(version['time'])
-        v['type'] = version['type']
+        v["id"] = version["id"]
+        v["time"] = version["time"]
+        v["niceTime"] = format_date(version["time"])
+        v["type"] = version["type"]
 
-        output['versions'].append(v)
+        output["versions"].append(v)
 
-        bykey[version['id']] = version['time']
+        bykey[version["id"]] = version["time"]
 
-    output['latest'] = {}
+    output["latest"] = {}
 
-    for version in versions['latest']:
-        output['latest'][version] = {}
+    for version in versions["latest"]:
+        output["latest"][version] = {}
 
-        output['latest'][version]['id'] = versions['latest'][version]
-        output['latest'][version]['time'] = bykey[versions['latest'][version]]
-        output['latest'][version]['niceTime'] = format_date(
-            bykey[versions['latest'][version]])
+        output["latest"][version]["id"] = versions["latest"][version]
+        output["latest"][version]["time"] = bykey[versions["latest"][version]]
+        output["latest"][version]["niceTime"] = format_date(
+            bykey[versions["latest"][version]])
 
     return jsonify(output)
 
 
-@app.route('/server/status')
+@app.route("/server/status")
 @jsonp
 def server_status():
-    ip = request.args.get('ip')
+    ip = request.args.get("ip")
 
     if not ip:
-        raise InvalidUsage('no ip')
+        raise InvalidUsage("no ip")
 
-    port = request.args.get('port')
+    port = request.args.get("port")
 
     if port is None:
         port = 25565
@@ -119,10 +124,10 @@ def server_status():
         try:
             port = int(port)
         except:
-            raise InvalidUsage('invalid port')
+            raise InvalidUsage("invalid port")
 
     m = md5()
-    m.update('%s%s' % (ip, port))
+    m.update("%s%s" % (ip, port))
     m = m.hexdigest()
 
     try:
@@ -132,23 +137,23 @@ def server_status():
 
     if not result:
         result = {}
-        result['time'] = time()
-        result['value'] = get_info(ip, port)
+        result["time"] = time()
+        result["value"] = get_info(ip, port)
 
-        set_cache_item(m, result['value'])
+        set_cache_item(m, result["value"])
 
-    if not result['value']:
+    if not result["value"]:
         return jsonify({
-            'status': 'success',
-            'online': False
+            "status": "success",
+            "online": False
         })
 
-    if request.args.get('favicon') is None or request.args.get('favicon') == 'false':
+    if request.args.get("favicon") is None or request.args.get("favicon") == "false":
         favicon = False
     else:
         favicon = True
 
-    if request.args.get('players') is not None or request.args.get('players') == 'true':
+    if request.args.get("players") is not None or request.args.get("players") == "true":
         players = True
     else:
         players = False
@@ -156,9 +161,9 @@ def server_status():
     return jsonify(parse_server_data(result, favicon, players))
 
 
-@app.route('/server/info')
+@app.route("/server/info")
 def server_info():
-    return 'nyi'
+    return "nyi"
 
 if __name__ == "__main__":
-    app.run(debug=True, host='::')
+    app.run(debug=True, host="::")
